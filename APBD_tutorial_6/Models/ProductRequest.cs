31d317eece6c586d30bbc0d8948fe8b2b7bc93ea
s23985 <s23985@pjwstk.edu.pl>
2024-06-24 19:35:25 +0200

    Add Required attribute in ProductRequest

--- APBD_tutorial_6/Models/ProductRequest.cs
+++ APBD_tutorial_6/Models/ProductRequest.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace APBD_tutorial_6.Models;
 
 public class ProductRequest
 {
+    [Required]
     public int IdProduct { get; set; }
+    [Required]
     public int IdWarehouse { get; set; }
+    [Required]
     public int Amount { get; set; }
+    [Required]
     public DateTime CreatedAt { get; set; }
 }
